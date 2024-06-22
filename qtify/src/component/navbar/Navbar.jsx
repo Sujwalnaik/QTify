942d32742d53eb2193d757c70d76517b8a4be4ea
Sujwalnaik <sujwalbn.a0@gmail.com>
2024-06-22 10:11:24 +0530

    modified navbar

--- qtify/src/component/navbar/Navbar.jsx
+++ qtify/src/component/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
         item
         xs={12}
         bgcolor={"#34C94B"}
-        padding={"9px"}
+        padding={"10px"}
         display={"flex"}
         justifyContent={"space-between"}
         alignItems={"center"}
@@ -20,54 +20,65 @@ function Navbar() {
           <img
             src={logo}
             alt="Logo"
-            style={{ width: "80px", marginLeft: "40px" }}
+            style={{ width: "80px", marginLeft: "20px" }}
           />
         </Grid>
 
-        <Grid container alignItems="center" justifyContent="center">
+        <Grid item xs={12} display="flex" justifyContent="center">
           <TextField
             type="search"
             placeholder="Search an album of your voice"
             variant="outlined"
             sx={{
-              fontSize: "15px",
-              width: "568px",
+              // width: "100%",
+              width: "40rem",
 
               "& .MuiOutlinedInput-root": {
                 borderRadius: "8px",
-                border: "1px solid #121212",
+                border: "1px solid black",
                 bgcolor: "white",
+              },
+              "&.Mui-focused": {
+                border: "none",
+                // boxShadow: "0 0 5px rgba(0,0,0,0.2)",
               },
             }}
             InputProps={{
               endAdornment: (
-                <InputAdornment
-                  position="end"
-                  sx={{ borderLeft: "1px solid black" }}
-                >
-                  <SearchIcon />
+                <InputAdornment position="end">
+                  <SearchIcon
+                    sx={{
+                      borderLeft: "2px solid grey",
+                      width: "auto",
+                      height: "30px",
+                      padding: "8px",
+                    }}
+                  />
                 </InputAdornment>
               ),
             }}
           />
         </Grid>
-        <Button
-          variant="contained"
-          sx={{
-            fontWeight: 600,
-            bgcolor: "black",
-            color: "#34C94B",
-            fontSize: "18px",
-            width: "200px",
-            borderRadius: "10px",
-            textTransform: "none",
-            "&:hover": {
+        <Grid>
+          <Button
+            variant="contained"
+            sx={{
+              fontWeight: 600,
               bgcolor: "black",
-            },
-          }}
-        >
-          Give FeedBack
-        </Button>
+              color: "#34C94B",
+              fontSize: "18px",
+              width: "200px",
+              borderRadius: "10px",
+              textTransform: "none",
+              marginRight: "20px",
+              "&:hover": {
+                bgcolor: "black",
+              },
+            }}
+          >
+            Give Feedback
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
